bfdf307a1f8e3ef93795e1636618fe4260de2416
Talia <taliabahar@yahoo.com>
2020-08-30 22:58:02 -0700

    Updates

---
+++ precache-manifest.ce8cbc3d75bb56809ebc56cb5b14a0b5.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8ecf81fdce7df5359938fdabe82cc3",
+    "url": "/Personal-Website/index.html"
+  },
+  {
+    "revision": "41959e65c599fa7888c5",
+    "url": "/Personal-Website/static/css/2.971e4490.chunk.css"
+  },
+  {
+    "revision": "90438378a5fd64097907",
+    "url": "/Personal-Website/static/css/main.258e45bc.chunk.css"
+  },
+  {
+    "revision": "41959e65c599fa7888c5",
+    "url": "/Personal-Website/static/js/2.08a6f789.chunk.js"
+  },
+  {
+    "revision": "90438378a5fd64097907",
+    "url": "/Personal-Website/static/js/main.8492217f.chunk.js"
+  },
+  {
+    "revision": "045284a5137f81179867",
+    "url": "/Personal-Website/static/js/runtime-main.107831b7.js"
+  },
+  {
+    "revision": "e0a26f772ffcd26a88fa6f5ad4ba2a12",
+    "url": "/Personal-Website/static/media/JAGScreen.e0a26f77.png"
+  },
+  {
+    "revision": "e0f94eae6661881586111c494dd3fc68",
+    "url": "/Personal-Website/static/media/PackTrackScreen.e0f94eae.png"
+  },
+  {
+    "revision": "0a3c031fecc4f8bda867367a86620a2a",
+    "url": "/Personal-Website/static/media/PetMatchScreen.0a3c031f.png"
+  },
+  {
+    "revision": "24c4711fde49a8f7a3e2821b2f673f79",
+    "url": "/Personal-Website/static/media/REScreen.24c4711f.png"
+  },
+  {
+    "revision": "325c4b8cfb0ea1e7cb9d7e7492ac0fba",
+    "url": "/Personal-Website/static/media/ScreenTimeScreen.325c4b8c.png"
+  },
+  {
+    "revision": "ba22d350cdb4d5bae694aa371605e354",
+    "url": "/Personal-Website/static/media/TWSScreen.ba22d350.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Personal-Website/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Personal-Website/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Personal-Website/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Personal-Website/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Personal-Website/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Personal-Website/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Personal-Website/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Personal-Website/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Personal-Website/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Personal-Website/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Personal-Website/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Personal-Website/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Personal-Website/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Personal-Website/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Personal-Website/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Personal-Website/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
